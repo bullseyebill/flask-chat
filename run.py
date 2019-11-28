2d51918b18c6a7516547c914d260b53bedca0a70
Rupert <rupertjhewson@gmail.com>
2019-11-28 15:25:45 +0000

    chat facility

--- run.py
+++ run.py
@@ -6,10 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello There!</h1"
+    """Main page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE"
+
+
+@app.route('/<username>') 
+def user(username):
+    return "Hi " + username  
 
 
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', "0.0.0.0"),
             port=int(os.getenv('PORT', 8000)),
             debug=True)
+
+
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return "{0}:  {1}".format(username,message)

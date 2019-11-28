aa141d4bdc8ba2de2737941bfe739c42d77143f8
Rupert <rupertjhewson@gmail.com>
2019-11-28 15:06:57 +0000

    updates

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello There!</h1"
 
-    if __name__ == "__main__":
-        app.run(host=os.getenv('IP', "0.0.0.0"),
-                port=int(os.getenv('PORT', 8000)),
-                debug=True)
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('IP', "0.0.0.0"),
+            port=int(os.getenv('PORT', 8000)),
+            debug=True)
